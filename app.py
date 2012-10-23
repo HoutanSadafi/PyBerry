2f482f4a6b29131bb66368420a21c705efe1d1d7
HoutanSadafi <certop18@hotmail.com>
2012-10-22 23:58:56 -0400

    Added listing of aggregate of posts per month

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, g, request, redirect, url_for, render_template
 from pymongo import Connection
 from view_helper import *
+import calendar as cal
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
@@ -10,11 +11,17 @@ def before_request():
 	conn = Connection('mongodb://localhost/')
 	g.conn = conn
 	g.db = conn.blueberry
-
+	get_posts_per_author_aggregate(g)
 
 @app.teardown_request
 def teardown_request(exception):
 	g.conn.close()
+
+
+def get_posts_per_author_aggregate(g):
+	aggregate = g.db.postsPerMonth.find()
+	output = [ { 'year' : int(item['_id']['year']), 'month' : int(item['_id']['month']), 'count' : int(item['value']['count']), 'month_name' : cal.month_name[int(item['_id']['month'])] } for item in aggregate]
+	g.posts_per_month = output
 
 @app.route('/')
 def index():
@@ -28,7 +35,7 @@ def index():
 		return redirect(url_for('about'))
 	else:
 		pagination = create_pagination(posts.count(), number_to_skip, page_size, page_number)
-		return render_template('index.html', posts=posts, pagination=pagination)
+		return render_template('index.html', posts=posts, pagination=pagination, posts_per_month = g.posts_per_month)
 
 @app.route('/about')
 def about():
